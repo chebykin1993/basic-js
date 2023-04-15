f7da56cdf23893c7eb9cb2f9926a57daea2efef2
chebykin1993 <104596657+chebykin1993@users.noreply.github.com>
2023-04-15 20:27:42 +0300

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  let newArr = arr
+  let pizda = ['--discard-next', '--discard-prev', '--double-next', '--double-prev']
+  if (Array.isArray(newArr) === false) {throw new Error('\'arr\' parameter must be an instance of the Array!')}
+
+  for (let hui of newArr) {
+    if (pizda.includes(hui) && newArr.indexOf(hui) === 0) {
+       return arr.slice(newArr.indexOf(hui) + 1, -1)
+    }
+    if (pizda.includes(hui) && newArr.indexOf(hui) === -1) {
+    return newArr.slice(0, newArr.length - 2)
+    }
+
+    if (hui === '--discard-next') {newArr.splice(newArr.indexOf(hui), 2)}
+    if (hui === '--discard-prev') {newArr.splice(newArr.indexOf(hui) - 1, 2)}
+    if (hui === '--double-next') {hui = newArr[newArr.indexOf(hui) + 1]}
+    if (hui === '--double-prev') {hui = newArr[newArr.indexOf(hui) - 1]}
+  }
+  return newArr
 }
 
 module.exports = {
